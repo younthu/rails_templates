b40b738a1355c0b50ad12589252d98d0b25c4af2
Zhiyong Yang <evil_eve_live@outlook.com>
2020-10-22 09:57:08 +0800

    if no User/AdminUser model, don't register api in routes

--- activeadmin_template/config/routes/api.rb
+++ activeadmin_template/config/routes/api.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-    devise_for :admin_users, ActiveAdmin::Devise.config
-    devise_for :users, controllers: { sessions: 'users/sessions' }
+    devise_for :admin_users, ActiveAdmin::Devise.config if defined? AdminUser  # please remember to create AdminUser model
+    devise_for :users, controllers: { sessions: 'users/sessions' } if defined? User # please remember to create User model
 
     namespace :api do
         scope :v1 do
             mount_devise_token_auth_for 'User', at: 'users', controllers: {
                 token_validations:  'api/users/token_validations',
                 sessions: 'api/users/sessions'
-            }
+            } if defined? User
 
             resource :login_by_code, only: %i[create] # 微信小程序登录api
         end
