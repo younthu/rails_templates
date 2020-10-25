af799c8affbaf2f1b4f716e4049b1aff9c77a15a
Zhiyong Yang <evil_eve_live@outlook.com>
2020-10-25 08:32:52 +0800

    refactor api controller's paginate helper method

--- activeadmin_template/app/controllers/api/base_controller.rb
+++ activeadmin_template/app/controllers/api/base_controller.rb
@@ -9,15 +9,14 @@ class Api::BaseController < ActionController::API
 
     current, total, per_page = collection.current_page, collection.total_pages, collection.limit_value
 
-    return [{
-                pagination: {
-                    current:  current,
-                    previous: (current > 1 ? (current - 1) : nil),
-                    next:     (current == total ? nil : (current + 1)),
-                    per_page: per_page,
-                    pages:    total,
-                    count:    collection.total_count
-                }
-            }, collection]
+    return {
+            pagination: {
+                current:  current,
+                previous: (current > 1 ? (current - 1) : nil),
+                next:     (current == total ? nil : (current + 1)),
+                per_page: per_page,
+                pages:    total,
+                count:    collection.total_count
+            }, items: collection}
   end
 end
