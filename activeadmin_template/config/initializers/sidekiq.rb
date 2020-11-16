fb87fa3a4e579f8ae5a1154ee08a912f0c503342
Zhiyong Yang <evil_eve_live@outlook.com>
2020-11-16 14:52:18 +0800

    update log initializer for sidekiq

--- activeadmin_template/config/initializers/sidekiq.rb
+++ activeadmin_template/config/initializers/sidekiq.rb
@@ -6,10 +6,9 @@ redis_namespace = Rails.application.secrets.redis_namespace
 Sidekiq.configure_server do |config|
   p redis_server
   config.redis = { url: "redis://#{redis_server}:#{redis_port}/#{redis_db_num}"}
-  # Rails.logger = Sidekiq::Logging.logger
+  Rails.logger = Sidekiq::Logging.logger
 end
 
 Sidekiq.configure_client do |config|
   config.redis = { url: "redis://#{redis_server}:#{redis_port}/#{redis_db_num}"}
-  # Rails.logger = Sidekiq::Logging.logger
 end
