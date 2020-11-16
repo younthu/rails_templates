7dbd7866934e45895bb72f3a7f7500c5da574be2
Zhiyong Yang <evil_eve_live@outlook.com>
2020-11-16 15:01:00 +0800

    update log initializer for sidekiq

--- activeadmin_template/config/initializers/sidekiq.rb
+++ activeadmin_template/config/initializers/sidekiq.rb
@@ -6,6 +6,9 @@ redis_namespace = Rails.application.secrets.redis_namespace
 Sidekiq.configure_server do |config|
   p redis_server
   config.redis = { url: "redis://#{redis_server}:#{redis_port}/#{redis_db_num}"}
+
+  # this is for Rails.logger.info/debug/warning. 
+  # If you don't want to change Rails.logger, you can use puts, puts will work in stdout.
   Rails.logger = Sidekiq::Logging.logger
 end
 
